15a933ce6c8cf9b7cc156ced45026a7bcc69ed22
imadken <imad.2002@yahoo.fr>
2023-12-01 23:17:11 +0100

    added more relevance models

--- back.py
+++ back.py
@@ -1,5 +1,8 @@
 import os
 import pandas as pd
+import nltk
+from math import sqrt,log10
+import re
 
 
 def load_processed_docs(folder_path):
@@ -39,3 +42,265 @@ def load_descripteurs_and_inverse():
     descripteurs.update(inverses)
 
     return descripteurs
+
+
+
+
+def split_tokenizer(txt):
+    return [token for token in txt.split()]
+
+def regex_tokenizer(txt,regex='(?:[A-Za-z]\.)+|[A-Za-z]+[\-@]\d+(?:\.\d+)?|\d+[A-Za-z]+|\d+(?:[\.\,]\d+)?%?|\w+(?:[\-/]\w+)*'):
+    reg = nltk.RegexpTokenizer(regex)
+    return reg.tokenize(txt)
+
+def stop_remove(tokens):
+    stop = nltk.corpus.stopwords.words('english')
+    return [term.lower() for term in tokens if term.lower() not in stop]
+
+def porter_stem(tokens):
+    porter = nltk.PorterStemmer()
+    return[porter.stem(term) for term in tokens]
+
+def lancester_stem(tokens):
+    # lancester = nltk.PorterStemmer()
+    lancester = nltk.LancasterStemmer()
+    return[lancester.stem(term) for term in tokens]
+
+
+def process_query(query,tokenize= "regex",stemming="Port"):
+    
+    """tokenize, remove stopwords and stem
+       
+       query : the searched sentence
+       tokenize : regex or split
+       stemming : port for porter else lancester
+       
+    Returns:
+        _type_: _description_
+    """
+    
+    tokens = regex_tokenizer(query) if tokenize.lower()=="regex" else split_tokenizer(query)
+    tokens = stop_remove(tokens)
+    tokens = porter_stem(tokens) if stemming.lower()== "port" else lancester_stem(tokens)
+    
+    return tokens
+
+def Scalar(query_tokens,inverse_doc):
+    result=dict()
+    # sum_w_i=dict()
+    for term in query_tokens:  
+        search_result = inverse_doc[inverse_doc["term"] == term]
+        for i in range(len(search_result)):
+           result[search_result.iloc[i,1]] = result.get(search_result.iloc[i,1],0) + search_result.iloc[i,-1]
+            #  sum_w_i[search_result.iloc[i,1]] = sum_w_i.get(search_result.iloc[i,1],0) + search_result.iloc[i,-1]
+    
+    
+    # for key , value in sum_w_i.items():
+        # result[key]  = value 
+   
+    # return result.items()        
+    return pd.DataFrame(result.items(),columns=["Doc","Relevance"])   
+
+
+def Cosine(query_tokens,inverse_doc):
+    
+    result=dict()
+    
+    sum_w_i = dict()
+    sum_w_i_2 = dict()
+    nbr_query_terms = dict()
+    
+    
+    for i in range(len(inverse_doc)):
+            sum_w_i_2[inverse_doc.iloc[i,1]] = sum_w_i_2.get(inverse_doc.iloc[i,1],0)+(pow(inverse_doc.iloc[i,-1],2))
+    
+    for term in query_tokens:  
+        search_result = inverse_doc[inverse_doc["term"] == term]
+
+        for i in range(len(search_result)):
+            sum_w_i[search_result.iloc[i,1]] = sum_w_i.get(search_result.iloc[i,1],0) + search_result.iloc[i,-1]
+            nbr_query_terms[search_result.iloc[i,1]] = nbr_query_terms.get(search_result.iloc[i,1],0) + 1
+            
+     
+    for key , value in sum_w_i.items():
+ 
+         
+        # result[key]  = value / (sqrt(sum_w_i_2[key])*sqrt(nbr_query_terms[key])) 
+        result[key]  = value / (sqrt(sum_w_i_2[key])*sqrt(len(query_tokens))) 
+        
+     
+         
+    # return result.items()        
+    return pd.DataFrame(result.items(),columns=["Doc","Relevance"]).reset_index(drop=True) 
+
+
+def Jaccard(query_tokens,inverse_doc):
+    result=dict()
+    
+    sum_w_i = dict()
+    sum_w_i_2 = dict()
+    nbr_query_terms = dict()
+    
+    for i in range(len(inverse_doc)):
+            sum_w_i_2[inverse_doc.iloc[i,1]] = sum_w_i_2.get(inverse_doc.iloc[i,1],0)+(pow(inverse_doc.iloc[i,-1],2))
+    
+    for term in query_tokens:  
+        search_result = inverse_doc[inverse_doc["term"] == term]
+        
+       
+        # sum_v_i_ = 0
+        
+        for i in range(len(search_result)):
+       
+            sum_w_i[search_result.iloc[i,1]] = sum_w_i.get(search_result.iloc[i,1],0) + search_result.iloc[i,-1]
+            nbr_query_terms[search_result.iloc[i,1]] = nbr_query_terms.get(search_result.iloc[i,1],0) + 1
+     
+    for key , value in sum_w_i.items():
+        
+        # result[key]  = value / ((nbr_query_terms[key])+(sum_w_i_2[key])-value) 
+        result[key]  = value / ((len(query_tokens))+(sum_w_i_2[key])-value) 
+       
+       
+         
+    # return result.items()        
+    return pd.DataFrame(result.items(),columns=["Doc","Relevance"]) 
+
+
+def doc_size(doc_name,inverse_doc):
+    # return len(inverse_doc[inverse_doc["doc"] == doc_name])
+    return inverse_doc[inverse_doc["doc"] == doc_name]["frequency"].sum()
+    
+    
+def docs_mean(inverse_doc):
+    
+    docs = inverse_doc["doc"].unique()
+    sum = 0
+    
+    for doc in docs:
+        sum += doc_size(doc,inverse_doc)
+    
+    return sum/len(docs)  
+  
+def docs_size(inverse_doc):
+    dl = dict()
+    docs = inverse_doc["doc"].unique()
+    for doc in docs:
+        dl[doc] = doc_size(doc,inverse_doc)
+    return dl    
+
+def BM25(query_tokens,inverse_doc,K=1.5,B=0.75,N=6):
+    
+    avdl = docs_mean(inverse_doc)
+    dl =docs_size(inverse_doc)
+    result = dict()
+    
+    for term in query_tokens: 
+        
+        search_result = inverse_doc[inverse_doc["term"] == term]
+        
+        frequency = dict()
+        n_i = len(search_result["doc"].unique())
+        
+        
+        for i in range(len(search_result)):
+            frequency[search_result.iloc[i,1]] = search_result.iloc[i,2]
+
+        for key , value in frequency.items():
+  
+           result[key]  = result.get(key,0) + ((value/(value+((B*dl[key]/avdl)+1-B)*K)) * log10((N-n_i+0.5)/(n_i+0.5)))
+         
+    # return result.items()        
+    return pd.DataFrame(result.items(),columns=["Doc","Relevance"]).reset_index(drop=True) 
+
+
+
+def is_valid_query(query):
+    
+    tokens = [f"'{token}'" if token.lower() not in ("and", "or", "not") else token.lower() for token in query.split()]
+    try:
+        eval(" ".join(tokens))
+    except SyntaxError:
+        valid = False
+    else:valid = True
+    
+    return valid
+
+
+def Bool_model(query,inverse_doc,stemming):
+    
+    if not is_valid_query(query):
+        return f" '{query}' is not a valid query"
+
+    # return f" '{query}' is a valid query"
+    query = query.lower()
+    #get terms
+    query_terms=[]
+    for token in query.split():
+        if token not in ("and", "or", "not"): query_terms.append(token)
+    #stemmer
+    tokens = porter_stem(query_terms) if stemming.lower()== "port" else lancester_stem(query_terms)   
+    
+    documents =dict()
+    
+    #for each document, append terms that exists
+    for term in tokens:
+         search_result = inverse_doc[inverse_doc["term"] == term]
+         for i in range(len(search_result)):
+            documents.setdefault(search_result.iloc[i,1], []).append(term)
+    # print(documents)
+    
+    results=dict()
+    
+    for key , doc_terms in documents.items():
+        
+            query_copy = query
+            for t in query_terms:
+                cont = False
+                for term in doc_terms:
+                    if term in t:
+                        cont =True
+                if cont: query_copy = query_copy.replace(t,"1") 
+                else:query_copy = query_copy.replace(t,"0")         
+            
+            try:
+               results[key] = eval(query_copy)   
+            except SyntaxError as e:
+                print(e)
+                return f" '{query}' is not a valid query"
+                    
+    # print(results)        
+            
+            
+     
+    return pd.DataFrame(results.items(),columns=["Doc","Relevance"]).reset_index(drop=True) 
+    # return f" '{query}' is  a valid query"     
+
+
+
+
+def RSV(query , processed_docs_dict , Vector_space_model,search_term,stemming):
+    
+    if Vector_space_model == "Scalar":
+        return Scalar(query,processed_docs_dict).sort_values(by=["Relevance"],ascending=False)
+    elif Vector_space_model == "Cosine":
+           
+        return Cosine(query,processed_docs_dict).sort_values(by=["Relevance"],ascending=False)
+    
+    elif Vector_space_model == "Jaccard" :
+       return Jaccard(query,processed_docs_dict).sort_values(by=["Relevance"],ascending=False)
+    
+    elif Vector_space_model == "BM25": 
+         return BM25(query,processed_docs_dict).sort_values(by=["Relevance"],ascending=False)
+    # # print(result_df)
+    elif Vector_space_model == "Bool" :
+            # return pd.DataFrame([is_valid_query(search_term)],columns=["valid"])    
+            return Bool_model(search_term,processed_docs_dict,stemming)    
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import gradio as gr
 import pandas as pd
-from back import load_descripteurs_and_inverse
+from back import load_descripteurs_and_inverse , process_query , Scalar ,Cosine , Jaccard ,RSV,BM25
+from re import escape
+import nltk
+porter_stemmer = nltk.PorterStemmer()
+lanc_stemmer = nltk.LancasterStemmer()
+
 
 # Sample data (replace this with your data)
 # data = {
@@ -18,36 +23,58 @@ from back import load_descripteurs_and_inverse
 processed_docs_dict = load_descripteurs_and_inverse()
 
 
-def search_engine(search_term, tokenization, stemming, display_option, grouping_option):
+def search_engine(search_term, tokenization, lancester, display_option, Search_Method,pertinence,Vector_space_model):
     global processed_docs_dict
 
     # retrieve inputs
 
     words = "regex" if tokenization == True else "split"
 
-    stem = "lancester" if stemming == True else "port"
+    stem = "lancester" if lancester == True else "port"
 
     collection = "inverse" if display_option == "docsperterm" else "descripteur"
+    
+    if pertinence == True and search_term!="":
+        
+        query = process_query(search_term,tokenize=words,stemming=stem)
+        result_df = RSV(query , processed_docs_dict[f"inverse_{words}_{stem}"] ,Vector_space_model,search_term,stem )
 
-    # output
-    if search_term == "":
-        result_df = processed_docs_dict[f"{collection}_{words}_{stem}"]
-
-    elif collection == "inverse":
-
-        result_df = processed_docs_dict[f"{collection}_{words}_{stem}"]
-        result_df = result_df[result_df["term"] == search_term]
-
+    
+    elif pertinence== True and search_term=="":
+           result_df=f"please enter a query"
+        
     else:
-        result_df = processed_docs_dict[f"{collection}_{words}_{stem}"]
-        result_df = result_df[result_df["doc"] == f"D{search_term}"]
-
-    # if display_option == 'docsperterm':
-    #     result_df = df[df['term'].str.contains(search_term, case=False)]
-    # else:
-    #     result_df = df[df['document_id'] == int(search_term)]
-
-    return result_df.to_html(index=False)
+         if lancester == True:
+            search_term = lanc_stemmer.stem(search_term)
+         else:
+            search_term = porter_stemmer.stem(search_term) 
+    # output
+         if search_term == "":
+             result_df = processed_docs_dict[f"{collection}_{words}_{stem}"]
+         
+         elif collection == "inverse":
+     
+             result_df = processed_docs_dict[f"{collection}_{words}_{stem}"]
+     
+             if Search_Method == "Whole word":
+                 result_df = result_df[result_df["term"] == search_term]
+             elif Search_Method == "Starts with":
+                 result_df = result_df[result_df["term"].str.startswith(
+                     search_term)]
+             else:
+                 result_df = result_df[result_df["term"].str.contains(
+                     escape(search_term))]
+                 
+         else:
+             result_df = processed_docs_dict[f"{collection}_{words}_{stem}"]
+             result_df = result_df[result_df["doc"] == f"D{search_term}"]
+             # print(f" {search_term} = {len(result_df)}")
+    
+         
+    if type(result_df)==str:
+        return result_df
+         
+    return result_df.reset_index().to_html(index=False)
     # return result_df[['doc', 'term', 'frequency', 'weight']].to_html(index=False)
 
 
@@ -55,14 +82,22 @@ iface = gr.Interface(
     fn=search_engine,
     inputs=[
         gr.Textbox(label="Search Term"),
-        gr.Checkbox("Tokenization"),
-        gr.Checkbox("Lancaster"),
+        gr.Checkbox(label="Tokenization",value=True),
+        gr.Checkbox(label="Lancaster"),
         gr.Radio(choices=["docsperterm", "termsperdoc"],
-                 label="display Option", value="termsperdoc")
-
+                 label="display Option", value="docsperterm"),
+        gr.Dropdown(label="Search Method", choices=[
+                    "Whole word", "Starts with", "Contains"], value="Contains"),
+        gr.Checkbox(label="Pertinence"),
+        gr.Dropdown(label="Vector space model", choices=[
+                    "Scalar", "Cosine", "Jaccard","BM25","Bool"], value="Scalar"),
     ],
     outputs=gr.HTML(),
-    live=True
+    live=True,
+    allow_flagging="never",
+    title="Search Engine",
+    theme=gr.themes.Soft()
 )
 
-iface.launch()
+iface.launch(inbrowser=True)
+
